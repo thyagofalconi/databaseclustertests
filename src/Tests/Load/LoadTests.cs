5e3c3dbeb2072ef11b067da45b7cb0fbd4267b85
Thyago Silva Falconi <thyago.falconi@sportsnewmedia.com>
2017-10-19 12:14:53 +0100

    Added unit test that runs a query from mongo

---
+++ src/Tests/Load/LoadTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using DatabaseClusterTests.Repository;
+using MongoDB.Bson.Serialization.Attributes;
+using NUnit.Framework;
+
+namespace DatabaseClusterTests.Tests.Load
+{
+    [TestFixture]
+    public class LoadTests
+    {
+        [Test]
+        public async Task Run()
+        {
+            //Given
+
+            const string databaseName = "DATABASE_NAME";
+            const string connectionString = "CONNECTION_STRING";
+
+            var databaseRepository = new MongoDbRepository(connectionString, databaseName);
+            
+            //When
+
+            var result = await databaseRepository.ExecuteExpensiveQuery<MyModel>();
+
+            //Then
+
+            Assert.IsTrue(result);
+        }
+
+        public class MyModel
+        {
+            [BsonId]
+            public object Id { get; set; }
+        }
+    }
+}
